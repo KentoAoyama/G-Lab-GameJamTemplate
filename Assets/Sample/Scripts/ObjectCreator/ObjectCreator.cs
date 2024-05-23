0dc8b1ddf810f48c7db81fe6feea80140e644a51
KentoAoyama <atoshi1129@gmail.com>
2024-05-23 23:53:22 +0900

    サンプルをまとめて作りました

--- Assets/Sample/Scripts/ObjectCreator/ObjectCreator.cs
+++ Assets/Sample/Scripts/ObjectCreator/ObjectCreator.cs
@@ -1,68 +1,78 @@
 using UnityEngine;
 
-// GameObjectの生成を行うクラス
+/// <summary>
+/// GameObjectの生成を行うクラス
+/// </summary>
 public class ObjectCreator : MonoBehaviour
 {
-    [Header("生成関連のパラメーター")]
+    [Header("生成したいオブジェクト")]
     [SerializeField]
-    ObjectCreateParamSO _createParam = default;
+    private GameObject[] _createObjects = default;
+
+    [Header("オブジェクトの種類をランダムに生成するか")]
+    [SerializeField]
+    private bool _isRandom = false;
+
+    [Header("自動で生成を行い続けるか")]
+    [SerializeField]
+    private bool _isAutoCreate = true;
+
+    [Header("生成のインターバル")]
+    [SerializeField]
+    private float _createInterval = 1.0f;
+
+    [Header("生成する個数制限")]
+    [SerializeField]
+    private bool _isCreateCountLimit = false;
+    [SerializeField]
+    private int _createCountNum = 10;
 
     [Header("以下、生成位置の指定に関するパラメーター")]
     [SerializeField]
-    private CreatePositionType _createPositionType = CreatePositionType.Random;
+    private CreatePositionType _createPositionType = CreatePositionType.RandomValue;
 
     [Header("Randomに生成する場合の範囲")]
     [SerializeField]
-    private Vector2 _createRange = new(10, 2);
+    private Vector3 _createRange = new(6, 0, 0);
 
     [Header("Transformで指定する場合の座標の設定")]
     [SerializeField]
     private Transform[] _createPositions = default;
+
+    [Header("生成したオブジェクトの親オブジェクト")]
+    [SerializeField]
+    private Transform _parentObject = default;
 
     /// <summary>
     /// 生成時の座標指定形式の種類
     /// </summary>
     private enum CreatePositionType
     {
-        Random,
-        Transform,
+        RandomValue,
+        RandomTransform,
     }
 
 
-    //========計測用メンバー変数========
+    //========システム用メンバー変数========
 
     // インターバル計測用
     private float _timeCount = 0.0f;
     // 生成したオブジェクトの個数カウント用
     private int _createCount = 0;
-
-
-    //========他のクラスから触る用のプロパティ========
-
     // オブジェクトを生成するかどうか
     private bool _isCreate = false;
-    /// <summary>
-    /// 他のクラスから生成するかどうかのフラグを変更する用プロパティ
-    /// </summary>
-    public bool IsCreate => _isCreate;
 
 
     private void Start()
     {
-        if (_createParam == null)
-        {
-            Debug.LogError("CreateParamSOが設定されていません");
-            return;
-        }
-
-        if (_createParam.CreateObjects.Length == 0)
+        if (_createObjects.Length == 0)
         {
             Debug.LogError("CreateParamSOにCreateObjectsが設定されていません");
             return;
         }
 
         // 生成するフラグを立てる
-        if (_createParam.IsAutoCreate)
+        if (_isAutoCreate)
         {
             _isCreate = true;
         }
@@ -80,7 +90,7 @@ public class ObjectCreator : MonoBehaviour
         _timeCount += Time.deltaTime;
 
         // インターバル時間に達したらオブジェクトを生成
-        if (_timeCount >= _createParam.CreateInterval)
+        if (_timeCount >= _createInterval)
         {
             CreateObject();
             _timeCount = 0.0f;
@@ -90,58 +100,84 @@ public class ObjectCreator : MonoBehaviour
     private void CreateObject()
     {
         // 生成する個数制限があるかチェック
-        if (_createParam.IsCreateCountLimit)
+        if (_isCreateCountLimit)
         {
-            if (_createParam.CreateCountNum <= 0)
+            if (_createCountNum <= 0)
             {
                 return;
             }
-            _createParam.CreateCountNum--;
+            _createCountNum--;
         }
 
         // 生成時の座標指定形式に応じて座標を設定
-        Vector2 createPos = Vector2.zero;
+        Vector3 createPos = Vector3.zero;
         switch (_createPositionType)
         {
-            case CreatePositionType.Random:
+            case CreatePositionType.RandomValue:
                 // ランダムな範囲にオブジェクトを生成
-                createPos = new Vector2(
+                createPos = new Vector3(
                     Random.Range(-_createRange.x, _createRange.x),
-                    Random.Range(-_createRange.y, _createRange.y));
+                    Random.Range(-_createRange.y, _createRange.y),
+                    Random.Range(-_createRange.z, _createRange.z));
                 break;
-            case CreatePositionType.Transform:
+            case CreatePositionType.RandomTransform:
                 // Transformで指定した座標にランダムにオブジェクトを生成
                 int index = Random.Range(0, _createPositions.Length);
-                createPos = new Vector2(
+                createPos = new Vector3(
                     _createPositions[index].position.x,
-                    _createPositions[index].position.y);
+                    _createPositions[index].position.y,
+                    _createPositions[index].position.z);
                 break;
         }
-        createPos += (Vector2)transform.position;
+        createPos += transform.position;
 
 
         GameObject createObj = null;
         // 生成するオブジェクトをランダムにするかチェック
-        if (_createParam.IsRandom)
+        if (_isRandom)
         {
             // CreateObjectの配列の中からランダムに選択
-            int index = Random.Range(0, _createParam.CreateObjects.Length);
-            createObj = _createParam.CreateObjects[index];
+            int index = Random.Range(0, _createObjects.Length);
+            createObj = _createObjects[index];
         }
         else
         {
             // CreateObjectの配列の中から順番に選択
-            int index = _createCount % _createParam.CreateObjects.Length;
-            createObj = _createParam.CreateObjects[index];
+            int index = _createCount % _createObjects.Length;
+            createObj = _createObjects[index];
+
+            // 生成したオブジェクトの個数カウントを加算
+            _createCount++;
         }
 
         // オブジェクトを生成
-        Instantiate(createObj, createPos, Quaternion.identity);
+        createObj = Instantiate(createObj, createPos, Quaternion.identity);
+
+        if (_parentObject != null)
+        {
+            createObj.transform.SetParent(_parentObject);
+        }
 
         // 自動で生成を行い続ける設定ではなかった場合、生成時にフラグを下げる
-        if (_createParam.IsAutoCreate == false)
+        if (_isAutoCreate == false)
         {
             _isCreate = false;
         }
+    }
+
+    /// <summary>
+    /// 生成フラグを変更する関数
+    /// </summary>
+    public void SetCreateFlag(bool isCreate)
+    {
+        _isCreate = isCreate;
+    }
+
+    /// <summary>
+    /// 生成インターバルを変更する関数
+    /// </summary>
+    public void SetCreateInterval(float interval)
+    {
+        _createInterval = interval;
     }
 }
